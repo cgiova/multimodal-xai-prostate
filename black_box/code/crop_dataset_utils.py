332b0d8ec8555463eb7bb8686e1e7e5365625870
Claudio Giovannoni <130065689+cgiova@users.noreply.github.com>
2023-11-13 11:21:10 +0100

    updated code in the files

--- black_box/code/crop_dataset_utils.py
+++ black_box/code/crop_dataset_utils.py
@@ -1,9 +1,33 @@
 import numpy as np
-import nibabel as nib
+import nibabel as nib #remember to install nibabel library in the notebook
 from skimage.transform import resize
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import os
+from tqdm import tqdm
+import re
+import glob
 
+# funtion to parse and obtain the key elements from the image filenames of the dataset
+def parse_image_filename(filename,class_label=True):
+    if class_label:
+        parts = filename.split('_')
+        patient_id = '_'.join(parts[:2])
+        study_id = parts[1]
+        image_type = parts[3]
+        class_label = int(re.search(r'class(\d+)', parts[-1]).group(1))
+        slice_index = int(re.search(rf'{image_type}(\d+)', filename).group(1))
+        return patient_id, study_id, image_type, class_label, slice_index
+    else:
+        filename = filename.split('.')[0]
+        parts = filename.split('_')
+        patient_id = '_'.join(parts[:2])
+        study_id = parts[1]
+        image_type = parts[-1]
+        slice_index = int(parts[2][3:])
+        return patient_id, study_id, image_type, slice_index
+
+# funtion to display a selected image
 def img_printer(img_path:str):
     img = mpimg.imread(img_path)
     print(f'Image shape: {img.shape}\n')
@@ -73,12 +97,238 @@ def get_crop_coordinates(delineation_path, num_slices):
 
     return rois, top_slice_indices.tolist()
 
-# function to save a chekpoint for the cropping operation, in order not to lose the progress in case
-# of runtine errors or discottections
-def save_checkpoint(counter, checkpoint_file):
-    with open(checkpoint_file, "w") as f:
-        f.write(str(counter))
-        print(f"Checkpoint saved at folder {counter}")
+# funtion to control if any of the newly created folders has anomalies i.e. more files than intended
+def check_anomalies(output_path:str, threshold:int):
+    # Initialize counters for total patient directories with anomalies and total anomalies
+    patient_dirs_with_anomalies = 0
+    total_anomalies = 0
+    total_positive_anomalies = 0
+    total_negative_anomalies = 0
+    tot_folders = 0
+    # Iterate through the patient directories
+    for patient_dir in os.listdir(output_path):
+        patient_path = os.path.join(output_path, patient_dir)
+        if not os.path.isdir(patient_path):
+            continue
+        tot_folders += 1
+        # Flag to track if any subdirectory has anomalies in the patient directory
+        has_anomalies_patient = False
+        count = 0
+        # Count the number of file paths within the subdirectory
+        num_files = len([f for f in os.listdir(patient_path) if os.path.isfile(os.path.join(patient_path, f))])
+        # Check if the number of file paths exceeds the threshold
+        if num_files > threshold:
+            count +=1
+            has_anomalies_patient = True
+            total_anomalies += (num_files - threshold)
+            total_positive_anomalies += (num_files - threshold)
+        elif num_files < threshold:
+            count +=1
+            has_anomalies_patient = True
+            total_anomalies += (num_files - threshold)
+            total_negative_anomalies += (abs(num_files - threshold))
+        # Check if any subdirectory in the patient directory has anomalies
+        if has_anomalies_patient:
+            patient_dirs_with_anomalies += 1
+            print(f"Patient directory '{patient_dir}' contains subdirectories with {count} anomalies.")
 
+    print(f"\nTotal number of patient directories: {tot_folders}")
+    print(f"Total number of patient directories with anomalies: {patient_dirs_with_anomalies}")
+    print(f"Total number of anomalies: {total_anomalies}")
+    print(f"Total number of positive anomalies: {total_positive_anomalies}")
+    print(f"Total number of negative anomalies: {total_negative_anomalies}")
 
+# funtion to check the number of folders, files and overall gb size of the data directory
+def file_checker(path:str):
+    if os.path.exists(path):
+        total_size = 0
+        num_patient_folders = 0
+        tot_files = 0
+        for dirpath, dirnames, filenames in tqdm(os.walk(path)):
+            for dirname in dirnames:
+                if dirname.isdigit():
+                    num_patient_folders += 1
 
+            for filename in filenames:
+                filepath = os.path.join(dirpath, filename)
+                total_size += os.path.getsize(filepath)
+                tot_files += 1
+
+        print(f'\nTotal number of patient folders: {num_patient_folders}')
+        print(f'Total number of patient files: {tot_files}')
+        print(f"Total size of {path.strip('/')[-1]}: {total_size / (1024 ** 3):.2f} Gigabytes")
+        return num_patient_folders
+    else:
+        print('Specified path does not exist')
+
+# funtion to crop the images and saved them in an output directory based on the coordinates and slices
+# obtained with the get_crop_coordinates(delineation_path, num_slices) funtion
+# used only for testing purposes, as it lacks a checkpoint logic
+def crop_dataset(input_path:str,output_root:str,crop_shape:tuple,num_slices:int, folder_breakpoint:int,
+                    wholegland_del_path:str, excluded_patients:list):
+    processed_folders = 1
+    print('Begin Dataset Creation!',"\n")
+    # Loop over the patient folders
+    for patient_folder in os.listdir(input_path):
+        if processed_folders % 5 == 0:
+            print(f"\nprocessing folder number... {processed_folders}: {patient_folder}\n")
+        else:
+            print(f"\nProcessing folder: {patient_folder} \n")
+        if patient_folder in excluded_patients:
+            continue
+        patient_path = os.path.join(input_path, patient_folder)
+        if os.path.isdir(patient_path):
+            # Create output directory fo each patient
+            output_path = os.path.join(output_root, patient_folder)
+            os.makedirs(output_path, exist_ok=True)
+            # Loop over the image files in the patient folder
+            for image_path in glob.glob(os.path.join(patient_path, '*.png')):
+                patient_filename = image_path.split('/')[-1]
+                patient_id, _, image_type, slice_num = parse_image_filename(patient_filename, class_label=False)
+                print(f"\npatient_filename {patient_filename}")
+                print(f"patient_id {patient_id}")
+                #print(f"study_id {study_id}")
+                print(f"image_type {image_type}")
+                print(f"slice_num {slice_num}\n")
+                # loop over to get the specific patient path
+                wholegland_del_patient_path = os.path.join(wholegland_del_path, patient_id + '.nii.gz')
+                try:
+                    rois, slices = get_crop_coordinates(wholegland_del_patient_path,num_slices)
+                    # check if the slice is in the list of positive delineations
+                    if slice_num in slices:
+                        # Get the coordinates for the current slice from the rois list
+                        coordinates_list = rois[slices.index(slice_num)]
+                        if re.search(r'_t2w\d{1,2}_t2w.png', image_path) or re.search(r'_t2w\d{1,2}_adc.png', image_path):
+                            t2w_img = mpimg.imread(image_path)
+                            t2w_img_crop = t2w_img[coordinates_list[0]:coordinates_list[1],\
+                                                    coordinates_list[2]:coordinates_list[3],:]
+                            # resize the image to a fixed format
+                            t2w_img_resized = resize(t2w_img_crop, crop_shape, preserve_range=True)
+                            # extract the image name without extension
+                            img_name = os.path.splitext(os.path.basename(image_path))[0]
+                            if img_name.endswith("_adc"):
+                                new_img_name = img_name.replace("_adc", "_t2w")
+                            else:
+                                new_img_name = img_name
+                            # Save the cropped image with the new filename
+                            output_img_path = os.path.join(output_path, new_img_name + '.png')
+                            # save the cropped image to the output directory
+                            plt.imsave(output_img_path, t2w_img_resized)
+                        # same logic for adc and hbv format images
+                        elif re.search(r'_adc\d{1,2}_adc.png', image_path):
+                            adc_img = mpimg.imread(image_path)
+                            adc_img_crop = adc_img[coordinates_list[0]:coordinates_list[1],\
+                                                    coordinates_list[2]:coordinates_list[3],:]
+                            adc_img_resized = resize(adc_img_crop, crop_shape, preserve_range=True)
+                            img_name = os.path.splitext(os.path.basename(image_path))[0]
+                            output_img_path = os.path.join(output_path, img_name + '.png')
+                            plt.imsave(output_img_path, adc_img_resized)
+                        elif re.search(r'_hbv\d{1,2}_hbv.png', image_path):
+                            hbv_img = mpimg.imread(image_path)
+                            hbv_img_crop = hbv_img[coordinates_list[0]:coordinates_list[1],\
+                                                    coordinates_list[2]:coordinates_list[3],:]
+                            hbv_img_resized = resize(hbv_img_crop, crop_shape, preserve_range=True)
+                            img_name = os.path.splitext(os.path.basename(image_path))[0]
+                            output_img_path = os.path.join(output_path, img_name + '.png')
+                            plt.imsave(output_img_path, hbv_img_resized)
+                except Exception as e:
+                    print(f"Error processing slice: {slice_num} in folder: {patient_folder}")
+                    print(f"Error message: {str(e)}")
+                    continue
+        # update counter
+        processed_folders += 1
+        # Break the loop if the desired number of folders have been processed
+        if folder_breakpoint:
+            if processed_folders >= folder_breakpoint:
+                print("\t\t",'Total folders processed...', processed_folders,"\n")
+                break
+    print("Dataset Creation Completed!")
+
+# funtion to crop the images and saved them in an output directory based on the coordinates and slices
+# obtained with the get_crop_coordinates(delineation_path, num_slices) funtion
+# includes a checkpoint system to resume if executions stops
+def crop_dataset_checkflag(input_path: str, output_root: str, crop_shape: tuple, num_slices: int,
+                            folder_breakpoint: int, wholegland_del_path:str, excluded_patients: list,
+                            checkpoint_file: str):
+    processed_folders = 1
+    print('Begin Dataset Creation!', "\n")
+    # check checkpoint
+    if checkpoint_file:
+        if os.path.exists(checkpoint_file):
+            with open(checkpoint_file, "r") as f:
+                checkpoint_counter = int(f.read())
+                print("\t", f"Checkpoint found! Resuming from folder number... {checkpoint_counter}")
+        else:
+            checkpoint_counter = 0
+            with open(checkpoint_file, "w") as f:
+                f.write(str(0))
+                print("\t", f"Checkpoint not found. Creating new Checkpoint")
+        # Loop over the patient folders
+        for patient_folder in os.listdir(input_path):
+            if processed_folders < checkpoint_counter:
+                processed_folders += 1
+                continue
+            if patient_folder in excluded_patients:
+                continue
+            if processed_folders % 1 == 0:
+                print(f"\n\t\tProcessing folder number... {processed_folders}: {patient_folder}")
+                with open(checkpoint_file,'w') as f:
+                    checkpoint_counter = processed_folders
+                    f.write(str(checkpoint_counter))
+            patient_path = os.path.join(input_path, patient_folder)
+            if os.path.isdir(patient_path):
+                output_path = os.path.join(output_root, patient_folder)
+                os.makedirs(output_path, exist_ok=True)
+                for image_path in glob.glob(os.path.join(patient_path, '*.png')):
+                    patient_filename = image_path.split('/')[-1]
+                    patient_id, _, image_type, slice_num = parse_image_filename(patient_filename, class_label=False)
+                    wholegland_del_patient_path = os.path.join(wholegland_del_path, patient_id + '.nii.gz')
+                    try:
+                        rois, slices = get_crop_coordinates(wholegland_del_patient_path, num_slices)
+                        if slice_num in slices:
+                            coordinates_list = rois[slices.index(slice_num)]
+                            if re.search(r'_t2w\d{1,2}_t2w.png', image_path) or re.search(
+                                    r'_t2w\d{1,2}_adc.png', image_path):
+                                t2w_img = mpimg.imread(image_path)
+                                t2w_img_crop = t2w_img[coordinates_list[0]:coordinates_list[1], \
+                                                        coordinates_list[2]:coordinates_list[3], :]
+                                t2w_img_resized = resize(t2w_img_crop, crop_shape, preserve_range=True)
+                                img_name = os.path.splitext(os.path.basename(image_path))[0]
+                                if img_name.endswith("_adc"):
+                                    new_img_name = img_name.replace("_adc", "_t2w")
+                                else:
+                                    new_img_name = img_name
+                                output_img_path = os.path.join(output_path, new_img_name + '.png')
+                                plt.imsave(output_img_path, t2w_img_resized)
+                            elif re.search(r'_adc\d{1,2}_adc.png', image_path):
+                                adc_img = mpimg.imread(image_path)
+                                adc_img_crop = adc_img[coordinates_list[0]:coordinates_list[1], \
+                                                        coordinates_list[2]:coordinates_list[3], :]
+                                adc_img_resized = resize(adc_img_crop, crop_shape, preserve_range=True)
+                                img_name = os.path.splitext(os.path.basename(image_path))[0]
+                                output_img_path = os.path.join(output_path, img_name + '.png')
+                                plt.imsave(output_img_path, adc_img_resized)
+                            elif re.search(r'_hbv\d{1,2}_hbv.png', image_path):
+                                hbv_img = mpimg.imread(image_path)
+                                hbv_img_crop = hbv_img[coordinates_list[0]:coordinates_list[1], \
+                                                        coordinates_list[2]:coordinates_list[3], :]
+                                hbv_img_resized = resize(hbv_img_crop, crop_shape, preserve_range=True)
+                                img_name = os.path.splitext(os.path.basename(image_path))[0]
+                                output_img_path = os.path.join(output_path, img_name + '.png')
+                                plt.imsave(output_img_path, hbv_img_resized)
+                    except Exception as e:
+                        # print(f"Error processing slice: {slice_num} in folder: {patient_folder}")
+                        # print(f"Error message: {str(e)}")
+                        continue
+                processed_folders += 1
+                if folder_breakpoint:
+                    if processed_folders >= folder_breakpoint:
+                        print("\t\t", 'Total folders processed...', processed_folders, "\n")
+                        break
+        # Save the final checkpoint
+        with open(checkpoint_file, 'w') as f:
+            f.write(str(processed_folders))
+        print("Dataset Creation Completed!")
+    else:
+        crop_dataset(input_path, output_root, crop_shape, num_slices, folder_breakpoint, wholegland_del_path,
+                        excluded_patients)
--- black_box/code/model_training_utils.py
+++ black_box/code/model_training_utils.py
@@ -1,13 +1,2 @@
 import re
 
-def parse_image_filename(filename):
-    #filename = filepath.split('/')[-1]
-    parts = filename.split('_')
-    patient_id = '_'.join(parts[:2])
-    image_type = parts[3]
-    class_label = int(re.search(r'class(\d+)', parts[-1]).group(1))
-    slice_index = int(re.search(rf'{image_type}(\d+)', filename).group(1))
-    return patient_id, image_type, class_label, slice_index
-
-
-
