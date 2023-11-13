98e7681aeeb18224aa9f6b77b50a0ac7c957f9b3
Claudio Giovannoni <130065689+cgiova@users.noreply.github.com>
2023-11-13 15:47:57 +0100

    added/adjusted functions in files

--- black_box/code/crop_data_utils.py
+++ black_box/code/crop_data_utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-import nibabel as nib #remember to install nibabel library in the notebook
+import nibabel as nib  # remember to install nibabel library in the notebook
 from skimage.transform import resize
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -8,8 +8,9 @@ from tqdm import tqdm
 import re
 import glob
 
+
 # funtion to parse and obtain the key elements from the image filenames of the dataset
-def parse_image_filename(filename,class_label=True):
+def parse_image_filename(filename, class_label=True):
     if class_label:
         parts = filename.split('_')
         patient_id = '_'.join(parts[:2])
@@ -27,12 +28,14 @@ def parse_image_filename(filename,class_label=True):
         slice_index = int(parts[2][3:])
         return patient_id, study_id, image_type, slice_index
 
+
 # funtion to display a selected image
-def img_printer(img_path:str):
+def img_printer(img_path: str):
     img = mpimg.imread(img_path)
     print(f'Image shape: {img.shape}\n')
     plt.imshow(img)
     plt.show()
+
 
 # function to get the cropping coordinates of the prostate gland trough the annotation file
 # this is needed to then crop the prostate image at correct ROI
@@ -44,12 +47,10 @@ def get_crop_coordinates(delineation_path, num_slices):
         print(f"Error loading NIfTI file: {delineation_path}")
         print(f"Error message: {str(e)}")
         return [], []
-
     img_resized = resize(img_fdata, (384, 384), preserve_range=True)
     cumsum_slices = np.sum(np.sum(img_resized, axis=0), axis=0)
     # Get indices of top slices based on overall density
     top_slice_indices = np.argsort(cumsum_slices)[-num_slices:][::-1]
-
     # List to store cropping coordinates
     rois = []
     for slice_idx in top_slice_indices:
@@ -59,7 +60,6 @@ def get_crop_coordinates(delineation_path, num_slices):
         rows, cols = np.where(mask)
         if len(rows) == 0 or len(cols) == 0:
             continue
-
         xmin, xmax = np.min(cols), np.max(cols)
         ymin, ymax = np.min(rows), np.max(rows)
         # Calculate the size of the bounding box
@@ -89,16 +89,15 @@ def get_crop_coordinates(delineation_path, num_slices):
         x_max = min(384, x_max)
         y_min = max(0, y_min)
         y_max = min(384, y_max)
-
         # Append the crop coordinates for the current slice to the list
         rois.append([x_min, x_max, y_min, y_max])
         # Print the slice number and relative density
-        #print(f"Slice {slice_idx}: Overall Density = {np.sum(annotation_slice)}")
-
+        # print(f"Slice {slice_idx}: Overall Density = {np.sum(annotation_slice)}")
     return rois, top_slice_indices.tolist()
 
+
 # funtion to control if any of the newly created folders has anomalies i.e. more files than intended
-def check_anomalies(output_path:str, threshold:int):
+def check_anomalies(output_path: str, threshold: int):
     # Initialize counters for total patient directories with anomalies and total anomalies
     patient_dirs_with_anomalies = 0
     total_anomalies = 0
@@ -118,12 +117,12 @@ def check_anomalies(output_path:str, threshold:int):
         num_files = len([f for f in os.listdir(patient_path) if os.path.isfile(os.path.join(patient_path, f))])
         # Check if the number of file paths exceeds the threshold
         if num_files > threshold:
-            count +=1
+            count += 1
             has_anomalies_patient = True
             total_anomalies += (num_files - threshold)
             total_positive_anomalies += (num_files - threshold)
         elif num_files < threshold:
-            count +=1
+            count += 1
             has_anomalies_patient = True
             total_anomalies += (num_files - threshold)
             total_negative_anomalies += (abs(num_files - threshold))
@@ -131,15 +130,15 @@ def check_anomalies(output_path:str, threshold:int):
         if has_anomalies_patient:
             patient_dirs_with_anomalies += 1
             print(f"Patient directory '{patient_dir}' contains subdirectories with {count} anomalies.")
-
     print(f"\nTotal number of patient directories: {tot_folders}")
     print(f"Total number of patient directories with anomalies: {patient_dirs_with_anomalies}")
     print(f"Total number of anomalies: {total_anomalies}")
     print(f"Total number of positive anomalies: {total_positive_anomalies}")
     print(f"Total number of negative anomalies: {total_negative_anomalies}")
 
+
 # funtion to check the number of folders, files and overall gb size of the data directory
-def file_checker(path:str):
+def file_checker(path: str):
     if os.path.exists(path):
         total_size = 0
         num_patient_folders = 0
@@ -148,12 +147,10 @@ def file_checker(path:str):
             for dirname in dirnames:
                 if dirname.isdigit():
                     num_patient_folders += 1
-
             for filename in filenames:
                 filepath = os.path.join(dirpath, filename)
                 total_size += os.path.getsize(filepath)
                 tot_files += 1
-
         print(f'\nTotal number of patient folders: {num_patient_folders}')
         print(f'Total number of patient files: {tot_files}')
         print(f"Total size of {path.strip('/')[-1]}: {total_size / (1024 ** 3):.2f} Gigabytes")
@@ -161,13 +158,14 @@ def file_checker(path:str):
     else:
         print('Specified path does not exist')
 
+
 # funtion to crop the images and saved them in an output directory based on the coordinates and slices
 # obtained with the get_crop_coordinates(delineation_path, num_slices) funtion
 # used only for testing purposes, as it lacks a checkpoint logic
-def crop_dataset(input_path:str,output_root:str,crop_shape:tuple,num_slices:int, folder_breakpoint:int,
-                    wholegland_del_path:str, excluded_patients:list):
+def crop_dataset(input_path: str, output_root: str, crop_shape: tuple, num_slices: int, folder_breakpoint: int,
+                    wholegland_del_path: str, excluded_patients: list):
     processed_folders = 1
-    print('Begin Dataset Creation!',"\n")
+    print('Begin Dataset Creation!', "\n")
     # Loop over the patient folders
     for patient_folder in os.listdir(input_path):
         if processed_folders % 5 == 0:
@@ -187,21 +185,22 @@ def crop_dataset(input_path:str,output_root:str,crop_shape:tuple,num_slices:int,
                 patient_id, _, image_type, slice_num = parse_image_filename(patient_filename, class_label=False)
                 print(f"\npatient_filename {patient_filename}")
                 print(f"patient_id {patient_id}")
-                #print(f"study_id {study_id}")
+                # print(f"study_id {study_id}")
                 print(f"image_type {image_type}")
                 print(f"slice_num {slice_num}\n")
                 # loop over to get the specific patient path
                 wholegland_del_patient_path = os.path.join(wholegland_del_path, patient_id + '.nii.gz')
                 try:
-                    rois, slices = get_crop_coordinates(wholegland_del_patient_path,num_slices)
+                    rois, slices = get_crop_coordinates(wholegland_del_patient_path, num_slices)
                     # check if the slice is in the list of positive delineations
                     if slice_num in slices:
                         # Get the coordinates for the current slice from the rois list
                         coordinates_list = rois[slices.index(slice_num)]
-                        if re.search(r'_t2w\d{1,2}_t2w.png', image_path) or re.search(r'_t2w\d{1,2}_adc.png', image_path):
+                        if re.search(r'_t2w\d{1,2}_t2w.png', image_path) or re.search(r'_t2w\d{1,2}_adc.png',
+                                                                                        image_path):
                             t2w_img = mpimg.imread(image_path)
-                            t2w_img_crop = t2w_img[coordinates_list[0]:coordinates_list[1],\
-                                                    coordinates_list[2]:coordinates_list[3],:]
+                            t2w_img_crop = t2w_img[coordinates_list[0]:coordinates_list[1], \
+                                                    coordinates_list[2]:coordinates_list[3], :]
                             # resize the image to a fixed format
                             t2w_img_resized = resize(t2w_img_crop, crop_shape, preserve_range=True)
                             # extract the image name without extension
@@ -217,16 +216,16 @@ def crop_dataset(input_path:str,output_root:str,crop_shape:tuple,num_slices:int,
                         # same logic for adc and hbv format images
                         elif re.search(r'_adc\d{1,2}_adc.png', image_path):
                             adc_img = mpimg.imread(image_path)
-                            adc_img_crop = adc_img[coordinates_list[0]:coordinates_list[1],\
-                                                    coordinates_list[2]:coordinates_list[3],:]
+                            adc_img_crop = adc_img[coordinates_list[0]:coordinates_list[1], \
+                                                    coordinates_list[2]:coordinates_list[3], :]
                             adc_img_resized = resize(adc_img_crop, crop_shape, preserve_range=True)
                             img_name = os.path.splitext(os.path.basename(image_path))[0]
                             output_img_path = os.path.join(output_path, img_name + '.png')
                             plt.imsave(output_img_path, adc_img_resized)
                         elif re.search(r'_hbv\d{1,2}_hbv.png', image_path):
                             hbv_img = mpimg.imread(image_path)
-                            hbv_img_crop = hbv_img[coordinates_list[0]:coordinates_list[1],\
-                                                    coordinates_list[2]:coordinates_list[3],:]
+                            hbv_img_crop = hbv_img[coordinates_list[0]:coordinates_list[1], \
+                                                    coordinates_list[2]:coordinates_list[3], :]
                             hbv_img_resized = resize(hbv_img_crop, crop_shape, preserve_range=True)
                             img_name = os.path.splitext(os.path.basename(image_path))[0]
                             output_img_path = os.path.join(output_path, img_name + '.png')
@@ -240,15 +239,16 @@ def crop_dataset(input_path:str,output_root:str,crop_shape:tuple,num_slices:int,
         # Break the loop if the desired number of folders have been processed
         if folder_breakpoint:
             if processed_folders >= folder_breakpoint:
-                print("\t\t",'Total folders processed...', processed_folders,"\n")
+                print("\t\t", 'Total folders processed...', processed_folders, "\n")
                 break
     print("Dataset Creation Completed!")
+
 
 # funtion to crop the images and saved them in an output directory based on the coordinates and slices
 # obtained with the get_crop_coordinates(delineation_path, num_slices) funtion
 # includes a checkpoint system to resume if executions stops
 def crop_dataset_checkflag(input_path: str, output_root: str, crop_shape: tuple, num_slices: int,
-                            folder_breakpoint: int, wholegland_del_path:str, excluded_patients: list,
+                            folder_breakpoint: int, wholegland_del_path: str, excluded_patients: list,
                             checkpoint_file: str):
     processed_folders = 1
     print('Begin Dataset Creation!', "\n")
@@ -272,7 +272,7 @@ def crop_dataset_checkflag(input_path: str, output_root: str, crop_shape: tuple,
                 continue
             if processed_folders % 1 == 0:
                 print(f"\n\t\tProcessing folder number... {processed_folders}: {patient_folder}")
-                with open(checkpoint_file,'w') as f:
+                with open(checkpoint_file, 'w') as f:
                     checkpoint_counter = processed_folders
                     f.write(str(checkpoint_counter))
             patient_path = os.path.join(input_path, patient_folder)
--- black_box/code/mapping_train_test_split_utils.py
+++ black_box/code/mapping_train_test_split_utils.py
@@ -1,40 +1,107 @@
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
-def custom_train_test_split(df, test_proportion=None, validation=True, random_state=None):
+
+# funtion used to perform the train test split or the train val test split in a way that there are no images of the
+# same patient across multiple sets (to avoid unwanted correlations during modelling and inference). Also,
+# the function assures a very similar class distribution across all sets
+def custom_train_test_split(df, test_size, val_size=None, random_state=None):
     unique_patient_ids = df['patient_id'].unique()
     total_samples = len(unique_patient_ids)
-
-    if validation:
-        # Calculate the test size based on the specified proportion
-        test_proportion = min(test_proportion, 0.5)  # Ensure test proportion is not greater than 0.5
-
-        test_size = int(test_proportion * total_samples)
-        validation_size = int(test_proportion * total_samples)
-
-        # Split patient_ids into train, validation, and test sets
-        train_patient_ids, remaining_patient_ids = train_test_split(unique_patient_ids, test_size=test_size * 2,
-                                                                    random_state=random_state)
-        valid_patient_ids, test_patient_ids = train_test_split(remaining_patient_ids, test_size=validation_size,
-                                                                random_state=random_state)
-
+    # Stratified split on patient_ids and corresponding labels
+    labels = df.groupby('patient_id')['label'].first()
+    train_patient_ids, remaining_patient_ids, _, _ = train_test_split(
+        unique_patient_ids, labels, test_size=test_size, stratify=labels, random_state=random_state)
+    if val_size is not None:
+        val_and_test_patient_ids, _, _, _ = train_test_split(
+            remaining_patient_ids, labels[remaining_patient_ids], test_size=val_size,
+            stratify=labels[remaining_patient_ids],
+            random_state=random_state)
+        valid_patient_ids, test_patient_ids, _, _ = train_test_split(
+            val_and_test_patient_ids, labels[val_and_test_patient_ids], test_size=0.5,
+            stratify=labels[val_and_test_patient_ids],
+            random_state=random_state)
         # Filter the dataframe based on the selected patient_ids
         train_df = df[df['patient_id'].isin(train_patient_ids)]
         valid_df = df[df['patient_id'].isin(valid_patient_ids)]
         test_df = df[df['patient_id'].isin(test_patient_ids)]
-
         return train_df, valid_df, test_df
     else:
-        # Calculate the test size based on the specified proportion
-        test_proportion = min(test_proportion, 0.5)  # Ensure test proportion is not greater than 0.5
-
-        test_size = int(test_proportion * total_samples)
-
-        # Randomly split patient_ids into train and test sets
-        train_patient_ids, test_patient_ids = train_test_split(unique_patient_ids, test_size=test_size * 2,
-                                                                random_state=random_state)
-
-        # Filter the dataframe based on the selected patient_ids
         train_df = df[df['patient_id'].isin(train_patient_ids)]
-        test_df = df[df['patient_id'].isin(test_patient_ids)]
-
+        test_df = df[df['patient_id'].isin(remaining_patient_ids)]
         return train_df, test_df
+
+
+# function used to check the newly created dfs with metrics such as size, class distribution, number of overlapping
+# patient across sets
+def check_dataframes(train_df,test_df,valid_df=None):
+    if valid_df is not None:
+        # check_overlapping
+        train_patient_ids = set(train_df['patient_id'].unique())
+        valid_patient_ids = set(valid_df['patient_id'].unique())
+        test_patient_ids = set(test_df['patient_id'].unique())
+        overlapping_train_valid = train_patient_ids.intersection(valid_patient_ids)
+        overlapping_train_test = train_patient_ids.intersection(test_patient_ids)
+        overlapping_valid_test = valid_patient_ids.intersection(test_patient_ids)
+        print('Train dataframe size:', len(train_df))
+        print('Validation dataframe size:', len(valid_df))
+        print('Test dataframe size:', len(test_df))
+        print('Train dataframe unique IDs:', train_df['patient_id'].nunique())
+        print('Validation dataframe unique IDs:', valid_df['patient_id'].nunique())
+        print('Test dataframe unique IDs:', test_df['patient_id'].nunique(),"\n")
+        print('Train dataframe samples:',train_df.head(),"\n")
+        print('Validation dataframe samples:',valid_df.head(),"\n")
+        print('Test dataframe samples:',test_df.head(),"\n")
+        if len(overlapping_train_valid) == 0:
+            print("No overlapping patient IDs between train and validation dataframes.")
+        else:
+            print("Overlapping patient IDs found between train and validation dataframes:")
+            print(overlapping_train_valid)
+        if len(overlapping_train_test) == 0:
+            print("No overlapping patient IDs between train and test dataframes.")
+        else:
+            print("Overlapping patient IDs found between train and test dataframes:")
+            print(overlapping_train_test)
+        if len(overlapping_valid_test) == 0:
+            print("No overlapping patient IDs between validation and test dataframes.")
+        else:
+            print("Overlapping patient IDs found between validation and test dataframes:")
+            print(overlapping_valid_test)
+        # checking class distribution in the dfs
+        for i, df in enumerate([train_df, test_df, valid_df]):
+            label_counts = df['label'].value_counts().sort_index()
+            total_samples = len(df)
+            percentage_per_class = (label_counts / total_samples) * 100
+
+            result_df = pd.DataFrame({
+                'Count': label_counts,
+                'Percentage': percentage_per_class
+            }).sort_index()
+            print(f"\nClass balance for dataframe {i + 1}:\n")
+            print(result_df)
+            print("\n" + "=" * 40 + "\n")
+    else:
+        train_patient_ids = set(train_df['patient_id'].unique())
+        test_patient_ids = set(test_df['patient_id'].unique())
+        overlapping_train_test = train_patient_ids.intersection(test_patient_ids)
+        print('Train dataframe size:', len(train_df))
+        print('Test dataframe size:', len(test_df))
+        print('Train dataframe unique IDs:', train_df['patient_id'].nunique())
+        print('Test dataframe unique IDs:', test_df['patient_id'].nunique(),"\n")
+        print('Train dataframe samples:',train_df.head(),"\n")
+        print('Test dataframe samples:',test_df.head(),"\n")
+        if len(overlapping_train_test) == 0:
+            print("No overlapping patient IDs between train and test dataframes.\n")
+        else:
+            print("Overlapping patient IDs found between train and test dataframes:", overlapping_train_test,"\n")
+        for i, df in enumerate([train_df,test_df]):
+            label_counts = df['label'].value_counts().sort_index()
+            total_samples = len(df)
+            percentage_per_class = (label_counts / total_samples) * 100
+            result_df = pd.DataFrame({
+                'Count': label_counts,
+                'Percentage': percentage_per_class
+            }).sort_index()
+            print(f"Class balance for dataframe {i + 1}:\n")
+            print(result_df)
+            print("\n" + "=" * 40 + "\n")
