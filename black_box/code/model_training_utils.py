e1724f2ee425e453dc31d5d5bcef21fef00780da
Claudio Giovannoni <130065689+cgiova@users.noreply.github.com>
2023-11-15 15:48:04 +0100

    upadated to funtions in files

--- black_box/code/model_training_utils.py
+++ black_box/code/model_training_utils.py
@@ -1,65 +1,46 @@
-import tensorflow as tf
-import keras
 import re
 import cv2
 import numpy as np
 import os
 from keras import backend as K
-from keras.backend import clip, log, mean
-from keras.callbacks import LearningRateScheduler
+from crop_data_utils import parse_image_filename
 
-def dataset_checker(train_folder_gdrive_0,train_folder_gdrive_1,
-                    valid_folder_gdrive_0,valid_folder_gdrive_1,
-                    test_folder_gdrive_0,test_folder_gdrive_1):
+#funtion to examine the dataset and check for issues in files
+def count_files_in_folders(*folder_paths):
+    total_files = 0
+    # Ensure an even number of folder paths
+    if len(folder_paths) % 2 != 0:
+        raise ValueError("Folder paths must be provided in pairs.")
+    for i in range(0, len(folder_paths), 2):
+        folder_0 = folder_paths[i]
+        folder_1 = folder_paths[i + 1]
+        _, _, files_0 = next(os.walk(folder_0))
+        _, _, files_1 = next(os.walk(folder_1))
+        file_count_0 = len(files_0)
+        file_count_1 = len(files_1)
+        file_count = file_count_0 + file_count_1
+        total_files += file_count
+        print(f'Total files in Folder Pair {i // 2 + 1}: {file_count}')
+        print(f'Files in Folder 0 of Pair {i // 2 + 1}: {file_count_0}, Files in Folder 1 of Pair {i // 2 + 1}: {file_count_1}\n')
+    print(f'Total files in all Folder Pairs: {total_files}')
 
-    _, _, files_train_0 = next(os.walk(train_folder_gdrive_0))
-    _, _, files_train_1 = next(os.walk(train_folder_gdrive_1))
 
-    _, _, files_valid_0 = next(os.walk(valid_folder_gdrive_0))
-    _, _, files_valid_1 = next(os.walk(valid_folder_gdrive_1))
-
-    _, _, files_test_0 = next(os.walk(test_folder_gdrive_0))
-    _, _, files_test_1 = next(os.walk(test_folder_gdrive_1))
-
-    file_count_tr_0 = len(files_train_0)
-    file_count_tr_1 = len(files_train_1)
-    file_count_tr = int(file_count_tr_0 + file_count_tr_1)
-
-    file_count_val_0 = len(files_valid_0)
-    file_count_val_1 = len(files_valid_1)
-    file_count_val = int(file_count_val_0 + file_count_val_1)
-
-    file_count_te_0 = len(files_test_0)
-    file_count_te_1 = len(files_test_1)
-    file_count_te = int(file_count_te_0 + file_count_te_1)
-
-    print(f'total files in GDrive: {file_count_tr + file_count_te + file_count_val}')
-    print(f'tot files in GDrive Training set: {file_count_tr}, of which {file_count_tr_0} label "0" and {file_count_tr_1} label "1"')
-    print(f'tot files in GDrive Validation set: {file_count_val}, of which {file_count_val_0} label "0" and {file_count_val_1} label "1"')
-    print(f'tot files in GDrive Test set: {file_count_te}, of which {file_count_te_0} label "0" and {file_count_te_1} label "1"')
-
-def parse_image_filename(filename):
-    #filename = filepath.split('/')[-1]
-    parts = filename.split('_')
-    patient_id = '_'.join(parts[:2])
-    study_id = parts[1]
-    image_type = parts[3]
-    class_label = int(re.search(r'class(\d+)', parts[-1]).group(1))
-    slice_index = int(re.search(rf'{image_type}(\d+)', filename).group(1))
-    return patient_id, study_id, image_type, class_label, slice_index
-
+# funtion to preprocess the input images before concatenating them into np arrays, customizable
 def preprocessing(image):
+    # pixel normalization
     image = np.uint16(image * 255.0)
     # Apply CLAHE
     clahe = cv2.createCLAHE(clipLimit=2, tileGridSize=(4,4))
     image = clahe.apply(image) + 30
     return image
 
+
+# funtion to select complementrary images of different modalities but same patient id and slice index, preprocess them,
+# and finally concatenate them into numpy arrays
 def array_stacking(folder_path, subset=''):
     image_pairs = {}
     stacked_arrays = []
     labels = []
-
     for root, dirs, files in os.walk(folder_path):
         for img_path in files:
             img_name = os.path.basename(img_path)
@@ -79,34 +60,30 @@ def array_stacking(folder_path, subset=''):
             t2w_image = cv2.imread(t2w_path, cv2.IMREAD_GRAYSCALE)
             adc_image = cv2.imread(adc_path, cv2.IMREAD_GRAYSCALE)
             hbv_image = cv2.imread(hbv_path, cv2.IMREAD_GRAYSCALE)
-
             t2w_image = preprocessing(t2w_image)
             adc_image = preprocessing(adc_image)
             hbv_image = preprocessing(hbv_image)
-
+            # possibility to apply data augmentation on taining images
             if subset == 'train':
                 t2w_array = np.array(t2w_image)
                 adc_array = np.array(adc_image)
                 hbv_array = np.array(hbv_image)
-
                 t2w_array = np.expand_dims(t2w_image, axis=-1)
                 adc_array = np.expand_dims(adc_image, axis=-1)
                 hbv_array = np.expand_dims(hbv_image, axis=-1)
-
                 stacked_arrays.append(np.concatenate([t2w_array, adc_array,hbv_array], axis=-1))
                 labels.append(label)
             else:
                 t2w_array = np.array(t2w_image)
                 adc_array = np.array(adc_image)
                 hbv_array = np.array(hbv_image)
-
                 t2w_array = np.expand_dims(t2w_image, axis=-1)
                 adc_array = np.expand_dims(adc_image, axis=-1)
                 hbv_array = np.expand_dims(hbv_image, axis=-1)
-
                 stacked_arrays.append(np.concatenate([t2w_array, adc_array,hbv_array], axis=-1))
                 labels.append(label)
     return np.array(stacked_arrays), np.array(labels)
+
 
 # Custom Weighted Binary Cross-Entropy Loss function
 def wbce(weight1, weight0):
@@ -118,6 +95,8 @@ def wbce(weight1, weight0):
         return K.mean(logloss, axis=-1)
     return loss
 
+
+# funtion wich implements learning rate scheduler to use with SGD optimization technique
 def scheduler(epoch, lr):
     if epoch < 50:
         return lr
