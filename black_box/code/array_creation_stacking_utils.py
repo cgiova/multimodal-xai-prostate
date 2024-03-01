46cfad5d5e0b1f3c3ca75c4f7686a6f1b7555e54
Claudio Giovannoni <130065689+cgiova@users.noreply.github.com>
2024-03-01 10:56:57 +0100

    minor changes

--- black_box/code/array_creation_stacking_utils.py
+++ black_box/code/array_creation_stacking_utils.py
@@ -5,9 +5,9 @@ import os
 import pickle as pk
 
 
-def imagepairs_dict_create(path: str):
+def create_imagepairs_dict(path: str):
     """
-    Create a dictionary object with image pairs (key,label):image _type
+    Create a dictionary object with image pairs (patient_id,label):image_type
     Used for stacking the correct images together in the array_stacking function
     """
     image_pairs = {}
@@ -16,8 +16,7 @@ def imagepairs_dict_create(path: str):
             img_name = os.path.basename(img_path)
             patient_id, study_id, image_type, label, slice_index = parse_image_filename2(img_name, class_label=True)
             key = (patient_id, slice_index)
-            image_pairs.setdefault(key, {})[image_type] = os.path.join(root,
-                                                                       img_path)
+            image_pairs.setdefault(key, {})[image_type] = os.path.join(root,img_path)
             image_pairs[key]['label'] = label
 
     return image_pairs
@@ -30,7 +29,6 @@ def patient_image_map(filepath: str):
     """
     with open(filepath, 'rb') as fp:
         mapping_dict = pk.load(fp)
-
     return mapping_dict
 
 
@@ -78,7 +76,58 @@ def preprocessing(image):
     return image
 
 
-def array_stacking(folder_path: str, mapping_dict_filepath: str, subset: str):
+def array_stacking(folder_path: str, subset: str, dims: int, mapping_dict_filepath=None):
+    """
+    Function creating the stacked arrays from the image modalities with or without tabular data
+    arrays are patient-wise and slice-wise for images, only patient-wise for tabular data turned into images
+    returns a numpy array for each patient slice, the modalities are stacked on the third dimension
+    """
+    if dims == 4 and mapping_dict_filepath is not None:
+        stacked_arrays, labels = array_stacking_4d(folder_path=folder_path,
+                                                   mapping_dict_filepath=mapping_dict_filepath,
+                                                   subset=subset)
+        return stacked_arrays, labels
+    else:
+        stacked_arrays = []
+        labels = []
+        lost_counter = 0
+        image_pairs = create_imagepairs_dict(folder_path)
+
+        # Pair and stack images
+        for key, image_data in image_pairs.items():
+            patient_id, slice_index = key
+            if 't2w' in image_data and 'adc' in image_data and 'hbv' in image_data:
+                t2w_path = image_data['t2w']
+                adc_path = image_data['adc']
+                hbv_path = image_data['hbv']
+                label = image_data['label']
+
+                # Load and preprocess the images as grayscale
+                t2w_array = np.array(cv2.imread(t2w_path, cv2.IMREAD_GRAYSCALE))
+                adc_array = np.array(cv2.imread(adc_path, cv2.IMREAD_GRAYSCALE))
+                hbv_array = np.array(cv2.imread(hbv_path, cv2.IMREAD_GRAYSCALE))
+
+                # possibility to apply data augmentation on training images
+                if subset == 'train':
+                    t2w_array = np.expand_dims(preprocessing(t2w_array), axis=-1)
+                    adc_array = np.expand_dims(preprocessing(adc_array), axis=-1)
+                    hbv_array = np.expand_dims(preprocessing(hbv_array), axis=-1)
+                    stacked_arrays.append(np.concatenate([t2w_array, adc_array, hbv_array], axis=-1))
+                    labels.append(label)
+                else:
+                    t2w_array = np.expand_dims(t2w_array, axis=-1)
+                    adc_array = np.expand_dims(adc_array, axis=-1)
+                    hbv_array = np.expand_dims(hbv_array, axis=-1)
+                    stacked_arrays.append(np.concatenate([t2w_array, adc_array, hbv_array], axis=-1))
+                    labels.append(label)
+            else:
+                lost_counter += 1
+                print(f"{patient_id} not found in mapping dict for subset: {subset}")
+        print(f"total lost files:{lost_counter}")
+        return np.array(stacked_arrays), np.array(labels)
+
+
+def array_stacking_4d(folder_path: str, mapping_dict_filepath: str, subset: str):
     """
     Function creating the stacked arrays from the other modalities and the tabular data
     arrays are patient-wise and slice-wise for images, only patient-wise for tabular data turned into images
@@ -88,7 +137,7 @@ def array_stacking(folder_path: str, mapping_dict_filepath: str, subset: str):
     labels = []
     lost_counter = 0
     mapping_dict = patient_image_map(mapping_dict_filepath)
-    image_pairs = imagepairs_dict_create(folder_path)
+    image_pairs = create_imagepairs_dict(folder_path)
 
     # Pair and stack images
     for key, image_data in image_pairs.items():
