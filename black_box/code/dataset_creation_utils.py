34cc70b33a69aea7f6c7569a1e3ba069df659c41
Claudio Giovannoni <130065689+cgiova@users.noreply.github.com>
2024-04-17 09:49:38 +0200

    updated notebook files

--- black_box/code/dataset_creation_utils.py
+++ black_box/code/dataset_creation_utils.py
@@ -1,16 +1,8 @@
 import os
 from PIL import Image
-import _pickle as cp
-from model_training_utils import parse_image_filename
 
-
-def dataset_creation(dataframe, destination_path: str):
-    """
-    Function to create the actual dataset from the mapping pandas dataframe, divided by class type
-    :param dataframe:Pandas: pandas dataframe with the label mapping information
-    :param destination_path: directory path where the dataset will be saved
-    :print: notification when process is completed
-    """
+# funtion to create the actual dataset from the mapping dataframes, divided by class type
+def dataset_creation(dataframe, destination_path:str):
     total_images = len(dataframe)
     processed_images = 0
     progress_updates = 0
@@ -31,8 +23,7 @@ def dataset_creation(dataframe, destination_path: str):
         filename_parts = os.path.splitext(image_filename)
         image_filename_with_class = f'{filename_parts[0]}_class{label}{filename_parts[1]}'
         # Construct the new image path in the destination folder
-        destination_path = os.path.join(clinically_relevant_folder if label == 1 else not_clinically_relevant_folder,
-                                        image_filename_with_class)
+        destination_path = os.path.join(clinically_relevant_folder if label == 1 else not_clinically_relevant_folder, image_filename_with_class)
         image = Image.open(image_path)
         # Save the converted image to the destination folder
         image.save(destination_path)
@@ -41,171 +32,3 @@ def dataset_creation(dataframe, destination_path: str):
             print(f'Processed {processed_images} images out of {total_images}.')
             progress_updates += 1
     print('Dataset creation completed.')
-
-
-def patient_3dimage_map(pickle_file_path: str):
-    """
-    Function opening the pickle file obtained from the IGTD algorithm
-    the file containts the original tabula data, the 3d array of the images generated
-    and the list of samples.
-    :param pickle_file_path:pickle: path to the pickle dump where the results of the tabular data conversion are stored
-    :return: dictionary with the mapping with {keys = patient_id ; values = 3d_image}
-    """
-    objects = []
-    with (open(pickle_file_path, "rb")) as openfile:
-        while True:
-            try:
-                objects.append(cp.load(openfile))
-            except EOFError:
-                break
-
-    # original_tab_data = objects[0]
-    generated_img_data = objects[1]
-    sample_names = objects[2]
-    mapping_dict = {}
-
-    for i in range(len(sample_names)):
-        patient_id = sample_names[i]
-        tabular_image_data = generated_img_data[:, :, i]
-        mapping_dict[patient_id] = tabular_image_data
-    return mapping_dict
-
-
-def preprocessing(image):
-    """
-    Function to preprocess the input images before concatenating them into np arrays, customizable
-    :param image: image to preprocess
-    :return: image preprocessed
-    """
-    # pixel normalization
-    image = np.uint16(image * 255.0)
-
-    # Apply CLAHE
-    clahe = cv2.createCLAHE(clipLimit=2, tileGridSize=(4, 4))
-    image = clahe.apply(image) + 30
-    return image
-
-
-def array_stacking(folder_path: str, subset=''):
-    """
-    Function creating the stacked arrays from the 3 modalities
-    arrays are patient-wise and slice-wise for images
-    returns a numpy array for each patient slice, the modalities are stacked on the third dimension
-    :param: folder_path: path to the folder containing the image dataset
-    :param: subset: specify the subset to be used (allows for data augmentation and preprocessing to the train set)
-    :return: tuple containing the stacked arrays and ground truth labels for training
-    """
-    image_pairs = {}
-    stacked_arrays = []
-    labels = []
-    for root, dirs, files in os.walk(folder_path):
-        for img_path in files:
-            img_name = os.path.basename(img_path)
-            patient_id, study_id, image_type, label, slice_index = parse_image_filename(img_name, class_label=True)
-            key = (patient_id, slice_index)
-            image_pairs.setdefault(key, {})[image_type] = os.path.join(root, img_path)
-            image_pairs[key]['label'] = label
-
-    # Pair and stack images
-    for key, image_data in image_pairs.items():
-        _, slice_index = key
-        if 't2w' in image_data and 'adc' in image_data and 'hbv' in image_data:
-            t2w_path = image_data['t2w']
-            adc_path = image_data['adc']
-            hbv_path = image_data['hbv']
-            label = image_data['label']
-
-            # Load and preprocess the images as grayscale
-            t2w_image = cv2.imread(t2w_path, cv2.IMREAD_GRAYSCALE)
-            adc_image = cv2.imread(adc_path, cv2.IMREAD_GRAYSCALE)
-            hbv_image = cv2.imread(hbv_path, cv2.IMREAD_GRAYSCALE)
-            t2w_image = preprocessing(t2w_image)
-            adc_image = preprocessing(adc_image)
-            hbv_image = preprocessing(hbv_image)
-
-            # possibility to apply data augmentation on training images
-            if subset == 'train':
-                t2w_array = np.expand_dims(np.array(t2w_image), axis=-1)
-                adc_array = np.expand_dims(np.array(adc_image), axis=-1)
-                hbv_array = np.expand_dims(np.array(hbv_image), axis=-1)
-                stacked_arrays.append(np.concatenate([t2w_array, adc_array, hbv_array], axis=-1))
-                labels.append(label)
-
-            else:
-                t2w_array = np.expand_dims(np.array(t2w_image), axis=-1)
-                adc_array = np.expand_dims(np.array(adc_image), axis=-1)
-                hbv_array = np.expand_dims(np.array(hbv_image), axis=-1)
-                stacked_arrays.append(np.concatenate([t2w_array, adc_array, hbv_array], axis=-1))
-                labels.append(label)
-
-    return np.array(stacked_arrays), np.array(labels)
-
-
-def array_stacking_tabular(folder_path: str, pickle_file_path: str, subset: str):
-    """
-    Function creating the stacked arrays from the 3 modalities and the tabular data
-    arrays are patient-wise and slice-wise for images, only patient-wise for tabular data turned into images
-    it return a numpy array for each patient slice, the modalities are stacked on the third dimension
-    :param: folder_path: path to the folder containing the image dataset
-    :param: pickle_file_path: path to the pickle dump where the results of the tabular data conversion are stored
-    :param: subset: specify the subset to be used (allows for data augmentation and preprocessing to the train set)
-    :return: tuple containing the stacked arrays and ground truth labels for training
-    """
-    image_pairs = {}
-    stacked_arrays = []
-    labels = []
-    mapping_dict = patient_3dimage_map(pickle_file_path)
-    lost_counter = 0
-    for root, dirs, files in os.walk(folder_path):
-        for img_path in files:
-            img_name = os.path.basename(img_path)
-            patient_id, study_id, image_type, label, slice_index = parse_image_filename(img_name, class_label=True)
-            key = (patient_id, slice_index)
-            image_pairs.setdefault(key, {})[image_type] = os.path.join(root, img_path)
-            image_pairs[key]['label'] = label
-
-    # Pair and stack images
-    for key, image_data in image_pairs.items():
-        patient_id, slice_index = key
-        if 't2w' in image_data and 'adc' in image_data and 'hbv' in image_data:
-            if patient_id in mapping_dict:
-                # check if patient is in mapping dictionary
-                t2w_path = image_data['t2w']
-                adc_path = image_data['adc']
-                hbv_path = image_data['hbv']
-                label = image_data['label']
-                # Load and preprocess the images as grayscale
-                t2w_image = cv2.imread(t2w_path, cv2.IMREAD_GRAYSCALE)
-                adc_image = cv2.imread(adc_path, cv2.IMREAD_GRAYSCALE)
-                hbv_image = cv2.imread(hbv_path, cv2.IMREAD_GRAYSCALE)
-
-                # access the tabular image data of the specific patient
-                image_tabular = mapping_dict[patient_id]
-                # possibility to apply data augmentation on training images
-                if subset == 'train':
-                    t2w_image = preprocessing(t2w_image)
-                    adc_image = preprocessing(adc_image)
-                    hbv_image = preprocessing(hbv_image)
-
-                    t2w_array = np.expand_dims(np.array(t2w_image), axis=-1)
-                    adc_array = np.expand_dims(np.array(adc_image), axis=-1)
-                    hbv_array = np.expand_dims(np.array(hbv_image), axis=-1)
-                    tab_img_array = np.expand_dims(image_tabular, axis=-1)
-
-                    stacked_arrays.append(np.concatenate([t2w_array, adc_array, hbv_array, tab_img_array], axis=-1))
-                    labels.append(label)
-
-                else:
-                    t2w_array = np.expand_dims(np.array(t2w_image), axis=-1)
-                    adc_array = np.expand_dims(np.array(adc_image), axis=-1)
-                    hbv_array = np.expand_dims(np.array(hbv_image), axis=-1)
-                    tab_img_array = np.expand_dims(image_tabular, axis=-1)
-
-                    stacked_arrays.append(np.concatenate([t2w_array, adc_array, hbv_array, tab_img_array], axis=-1))
-                    labels.append(label)
-        else:
-            lost_counter += 1
-            print(f"{patient_id} not found in mapping dict")
-
-    print(f"total lost files:{lost_counter}")
-    return np.array(stacked_arrays), np.array(labels)
--- black_box/code/model_training_utils.py
+++ black_box/code/model_training_utils.py
@@ -1,30 +1,12 @@
+import re
 import cv2
 import numpy as np
 import os
 from keras import backend as K
 from crop_data_utils import parse_image_filename
-from keras.backend import clip, log, mean
-from keras.layers import (Input, Conv2D, BatchNormalization, Activation, Add, GlobalAveragePooling2D, Dense,LeakyReLU,
-                          Dropout, MaxPooling2D)
-from keras.regularizers import l2
-from keras.models import Model
-from sklearn.metrics import roc_curve, auc
-import matplotlib.pyplot as plt
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-import seaborn as sns
-import numpy as np
-from imblearn.over_sampling import RandomOverSampler
-from imblearn.under_sampling import RandomUnderSampler
 
-
-#
-def count_files_in_folders(*folder_paths: str):
-    """
-    Function to examine the dataset and check for issues in files
-    :param folder_paths: filepath to folders to analyze
-    :return: total files number in folder paths
-    """
+#funtion to examine the dataset and check for issues in files
+def count_files_in_folders(*folder_paths):
     total_files = 0
     # Ensure an even number of folder paths
     if len(folder_paths) % 2 != 0:
@@ -43,13 +25,68 @@ def count_files_in_folders(*folder_paths: str):
     print(f'Total files in all Folder Pairs: {total_files}')
 
 
-def wbce(weight1: int, weight0: int):
-    """
-    Function for custom Weighted Binary Cross-Entropy Loss
-    :param weight1: weight for first parameter
-    :param weight0: weight for second parameter
-    :return: the loss function
-    """""
+# funtion to preprocess the input images before concatenating them into np arrays, customizable
+def preprocessing(image):
+    # pixel normalization
+    image = np.uint16(image * 255.0)
+    # Apply CLAHE
+    clahe = cv2.createCLAHE(clipLimit=2, tileGridSize=(4,4))
+    image = clahe.apply(image) + 30
+    return image
+
+
+# funtion to select complementary images of different modalities but same patient id and slice index, preprocess them,
+# and finally concatenate them into numpy arrays
+def array_stacking(folder_path, subset=''):
+    image_pairs = {}
+    stacked_arrays = []
+    labels = []
+    for root, dirs, files in os.walk(folder_path):
+        for img_path in files:
+            img_name = os.path.basename(img_path)
+            patient_id, image_type, label, slice_index = parse_image_filename(img_name, class_label=False)
+            key = (patient_id, slice_index)
+            image_pairs.setdefault(key, {})[image_type] = os.path.join(root, img_path)
+            image_pairs[key]['label'] = label
+    # Pair and stack images
+    for key, image_data in image_pairs.items():
+        patient_id, slice_index = key
+        if 't2w' in image_data and 'adc' in image_data and 'hbv' in image_data:
+            t2w_path = image_data['t2w']
+            adc_path = image_data['adc']
+            hbv_path = image_data['hbv']
+            label = image_data['label']
+            # Load and preprocess the images as grayscale
+            t2w_image = cv2.imread(t2w_path, cv2.IMREAD_GRAYSCALE)
+            adc_image = cv2.imread(adc_path, cv2.IMREAD_GRAYSCALE)
+            hbv_image = cv2.imread(hbv_path, cv2.IMREAD_GRAYSCALE)
+            t2w_image = preprocessing(t2w_image)
+            adc_image = preprocessing(adc_image)
+            hbv_image = preprocessing(hbv_image)
+            # possibility to apply data augmentation on training images
+            if subset == 'train':
+                t2w_array = np.array(t2w_image)
+                adc_array = np.array(adc_image)
+                hbv_array = np.array(hbv_image)
+                t2w_array = np.expand_dims(t2w_image, axis=-1)
+                adc_array = np.expand_dims(adc_image, axis=-1)
+                hbv_array = np.expand_dims(hbv_image, axis=-1)
+                stacked_arrays.append(np.concatenate([t2w_array, adc_array,hbv_array], axis=-1))
+                labels.append(label)
+            else:
+                t2w_array = np.array(t2w_image)
+                adc_array = np.array(adc_image)
+                hbv_array = np.array(hbv_image)
+                t2w_array = np.expand_dims(t2w_image, axis=-1)
+                adc_array = np.expand_dims(adc_image, axis=-1)
+                hbv_array = np.expand_dims(hbv_image, axis=-1)
+                stacked_arrays.append(np.concatenate([t2w_array, adc_array,hbv_array], axis=-1))
+                labels.append(label)
+    return np.array(stacked_arrays), np.array(labels)
+
+
+# Custom Weighted Binary Cross-Entropy Loss function
+def wbce(weight1, weight0):
     def loss(y_true, y_pred):
         y_true = K.cast(y_true, dtype='float32')
         y_true = K.clip(y_true, K.epsilon(), 1 - K.epsilon())
@@ -58,269 +95,14 @@ def wbce(weight1: int, weight0: int):
         return K.mean(logloss, axis=-1)
     return loss
 
-def scheduler(epoch: int, lr= 1e-3):
-    """
-    Learning rate is scheduled to be reduced after 80, 120, 160, 180 epochs.
-    Called automatically every epoch as part of callbacks during training.
-    :param epoch: The number of epochs
-    :param lr: the initial learning rate
-    :return lr (float32): the learning rate
-    """
-    if epoch > 160:
-        lr *= 0.5e-3
-    elif epoch > 120:
-        lr *= 1e-3
-    elif epoch > 80:
-        lr *= 1e-2
-    elif epoch > 40:
-        lr *= 1e-1
-    print('Learning rate: ', lr)
-    return lr
-#
-def scheduler_alt(epoch: int, lr: int, lr_decay=0.1):
-    """
-    function which implements an alternative learning rate scheduler to use with SGD optimization technique
-    :param lr_decay:
-    :param epoch: at which epoch or step epoch we want to schedule the learning rate change
-    :param lr: the initial learning rate
-    :param lr_decay: decay
-    :return:
-    """
+
+# funtion wich implements learning rate scheduler to use with SGD optimization technique
+def scheduler(epoch, lr):
     if epoch < 50:
         return lr
     elif epoch % 50 == 0:
-        lr *= lr_decay
+        lr *= 0.1
         return lr
     else:
         return lr
 
-def residual_block(x, filters, stride=1, first_block=False):
-    """
-    Function for the Residual Block
-    :param x:
-    :param filters:
-    :param stride:
-    :param first_block:
-    :return:
-    """
-    weight_decay=1e-4
-
-    # Shortcut connection
-    shortcut = x
-
-    # First convolution layer
-    x = Conv2D(filters, kernel_size=(3, 3), strides=stride, padding='same', kernel_regularizer=l2(weight_decay))(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-
-    # Second convolution layer
-    x = Conv2D(filters, kernel_size=(3, 3), padding='same', kernel_regularizer=l2(weight_decay))(x)
-    x = BatchNormalization()(x)
-
-    # Adjusting dimensions of the shortcut to match the main path
-    if first_block:
-        shortcut = Conv2D(filters, kernel_size=(1, 1), strides=stride, padding='same', kernel_regularizer=l2(weight_decay))(shortcut)
-        shortcut = BatchNormalization()(shortcut)
-
-    # Adding shortcut to the main path
-    x = Add()([x, shortcut])
-    x = Activation('relu')(x)
-
-    return x
-
-
-def resnet_v2_20(input_shape=input_shape, num_classes=1):
-    """
-    Function for ResNet-V2 model architecture
-    :param input_shape: shape of the input images
-    :param num_classes: number of classification labels
-    :return: the model
-    """
-    inputs = Input(shape=input_shape)
-    weight_decay=1e-4
-
-    # Initial convolution layer
-    x = Conv2D(16, kernel_size=(3, 3), padding='same', kernel_regularizer=l2(weight_decay))(inputs)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-
-    # Stacking residual blocks
-    num_blocks = [3, 3, 3]
-    filters = [16, 32, 64]
-
-    for stage in range(3):
-        for block in range(num_blocks[stage]):
-            stride = 1
-            if stage > 0 and block == 0:
-                stride = 2
-
-            x = residual_block(x, filters[stage], stride=stride, first_block=(block == 0))
-
-    # Global average pooling
-    x = GlobalAveragePooling2D()(x)
-
-    # Fully connected layers for classification
-    x = Dense(256, activation='relu')(x)
-    x = Dense(num_classes, activation='sigmoid')(x)
-
-    model = Model(inputs, x, name='resnet_v2_20')
-    return model
-
-def cnn(input_shape=input_shape, num_classes=1):
-    """
-    Function for a simple CNN architecture
-    :param input_shape: shape of the input images
-    :param num_classes: number of classification labels
-    :return: the model
-    """
-    inputs = Input(shape=input_shape)
-
-    # Block 1
-    x = Conv2D(64, (3, 3), padding='same')(inputs)
-    x = BatchNormalization()(x)
-    x = LeakyReLU(alpha=0.3)(x)
-    x = MaxPooling2D((2, 2))(x)
-
-    # Block 2
-    x = Conv2D(128, (3, 3), padding='same')(x)
-    x = BatchNormalization()(x)
-    x = LeakyReLU(alpha=0.3)(x)
-    x = MaxPooling2D((2, 2))(x)
-
-    # Block 3
-    x = Conv2D(256, (3, 3), padding='same')(x)
-    x = BatchNormalization()(x)
-    x = LeakyReLU(alpha=0.3)(x)
-    x = MaxPooling2D((2, 2))(x)
-
-    # Global average pooling and dense layers
-    x = GlobalAveragePooling2D()(x)
-    x = Dropout(0.5)(x)
-    x = Dense(128)(x)
-    x = LeakyReLU(alpha=0.3)(x)
-    x = Dropout(0.3)(x)
-    outputs = Dense(num_classes, activation='sigmoid')(x)
-
-    model = Model(inputs, outputs)
-    return model
-
-
-
-def plot_roc_auc(test_data,y_test,valid_data,y_valid):
-    """
-    Function plotting the ROC curve and the AUC for the validation set and test set
-    :param y_valid:
-    :param y_test:
-    :param test_data: test data
-    :param valid_data: validation data
-    :return: the plotted ROC curve
-    """
-    # Make predictions on the test and validation sets
-    predicted_probs_te = model.predict(test_data)
-    predicted_probs_val = model.predict(valid_data)
-
-    # Calculate ROC curve and AUC for test set
-    fpr_te, tpr_te, thresholds_te = roc_curve(y_test, predicted_probs_te)
-    roc_auc_te = auc(fpr_te, tpr_te)
-
-    # Calculate ROC curve and AUC for validation set
-    fpr_val, tpr_val, thresholds_val = roc_curve(y_valid, predicted_probs_val)
-    roc_auc_val = auc(fpr_val, tpr_val)
-
-    # Plot ROC curve for both test and validation sets
-    plt.figure(figsize=(8, 6))
-
-    plt.plot(fpr_te, tpr_te, color='darkorange', lw=2, label=f'Test (AUC = {roc_auc_te:.2f})')
-    plt.plot(fpr_val, tpr_val, color='navy', lw=2, label=f'Validation (AUC = {roc_auc_val:.2f})')
-
-    plt.plot([0, 1], [0, 1], color='black', lw=2, linestyle='--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver Operating Characteristic (ROC) Curve')
-    plt.legend(loc='lower right')
-    plt.show()
-
-def show_classification_report(test_data,y_test,valid_data,y_valid):
-    """
-    :param y_valid:
-    :param y_test:
-    :param test_data: test data
-    :param valid_data: validation data
-    :return:
-    """
-    data_list = [(valid_data, y_valid), (test_data, y_test)]
-
-    for data in data_list:
-
-        # Predict on the dataset using the model
-        predictions = model.predict(data[0])
-        predicted_labels = np.round(predictions).astype(int)
-
-        # Generate the classification report
-        report = classification_report(data[1], predicted_labels, target_names=['Class 0', 'Class 1'])
-
-        if data == data_list[0]:
-            print('\nValidation\n', report)
-        else:
-            print('\nTest\n', report)
-
-def plot_confusion_matrix(y_test):
-    """
-    Function plotting the confusion matrix for the test set
-    :param y_test:
-    :return:
-    """
-    # Make predictions on the test set
-    predicted_proba = model.predict(y_test)
-    predicted_labels = (predicted_proba > 0.5).astype(int)  # Assuming binary classification with a threshold of 0.5
-
-    # Calculate the confusion matrix
-    conf_matrix = confusion_matrix(y_test, predicted_labels)
-
-    # Create a heatmap of the confusion matrix
-    plt.figure(figsize=(5, 5))
-    sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues',
-                xticklabels=['Predicted 0', 'Predicted 1'],
-                yticklabels=['True 0', 'True 1'])
-    plt.xlabel('Predicted Labels')
-    plt.ylabel('True Labels')
-    plt.title('Confusion Matrix')
-    plt.show()
-
-
-def balance_dataset(X, y, method='oversample'):
-    """
-    Balances the dataset by either undersampling or oversampling.
-    Parameters:
-    X (numpy.ndarray): Feature matrix.
-    y (numpy.ndarray): Target array.
-    method (str): Method to balance dataset ('undersample' or 'oversample').
-    Returns:
-    X_resampled (numpy.ndarray): Resampled feature matrix.
-    y_resampled (numpy.ndarray): Resampled target array.
-    """
-    # Store the original sample dimension for later use
-    sample_dimension = X.shape[1:]
-    # Flatten X for resampling
-    X = X.reshape(X.shape[0], -1)
-
-    unique, counts = np.unique(y, return_counts=True)
-    print("Original dataset shape:", dict(zip(unique, counts)))
-
-    if method == 'oversample':
-        resampler = RandomOverSampler(random_state=0)
-    elif method == 'undersample':
-        resampler = RandomUnderSampler(random_state=0)
-    else:
-        raise ValueError("Method must be 'oversample' or 'undersample'")
-
-    X_resampled, y_resampled = resampler.fit_resample(X, y)
-    # Reshape X_resampled to have the original sample dimensions
-    X_resampled = X_resampled.reshape(-1, *sample_dimension)
-
-    unique_resampled, counts_resampled = np.unique(y_resampled, return_counts=True)
-    print("Resampled dataset shape:", dict(zip(unique_resampled, counts_resampled)))
-
-    return X_resampled, y_resampled
